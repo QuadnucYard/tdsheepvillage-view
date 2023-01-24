0bd52e94da2f67806efaca3bb478c9de02dd984c
QuadnucYard <2380433991@qq.com>
2023-01-24 18:54:24 +0800

    Add basic files

---
+++ src/main.ts
@@ -0,0 +1,21 @@
+import { createApp } from "vue";
+import App from "./App.vue";
+import router from "./router";
+import store from "./store";
+
+import ElementPlus from "element-plus";
+import "element-plus/theme-chalk/src/message.scss";
+
+import "./styles/index.scss";
+
+import * as ElementPlusIconsVue from "@element-plus/icons-vue";
+
+const app = createApp(App);
+app.use(router).use(ElementPlus).use(store);
+console.log(app);
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+  app.component(key, component);
+}
+
+app.mount("#app");
