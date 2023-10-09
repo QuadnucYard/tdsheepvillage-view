d84f949201aabc5ee27f250dc04ff925be1de33f
QuadnucYard <2380433991@qq.com>
2023-10-09 23:53:01 +0800

    Update packages

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
-import store from "./store";
 
 import ElementPlus from "element-plus";
 import "element-plus/theme-chalk/src/message.scss";
@@ -12,7 +11,7 @@ import "tailwindcss/tailwind.css";
 import * as ElementPlusIconsVue from "@element-plus/icons-vue";
 
 const app = createApp(App);
-app.use(router).use(ElementPlus).use(store);
+app.use(router).use(ElementPlus);
 console.log(app);
 
 for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
