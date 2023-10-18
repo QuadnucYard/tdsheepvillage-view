6505def2717716f17a2231eddbf58df67f4e56f0
QuadnucYard <2380433991@qq.com>
2023-10-18 09:16:05 +0800

    Add auto-import to git

--- vite.config.ts
+++ vite.config.ts
@@ -49,7 +49,7 @@ export default defineConfig({
         IconsResolver({ prefix: "Icon" }),
       ],
 
-      dts: path.resolve(pathSrc, "auto-imports.d.ts"),
+      dts: "src/auto-imports.d.ts",
     }),
 
     Components({
