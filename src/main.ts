fa4c16778a424a3faaefb3b9c535405a0fdecbe7
QuadnucYard <2380433991@qq.com>
2023-11-06 15:04:47 +0800

    Remove icon install

--- src/main.ts
+++ src/main.ts
@@ -8,16 +8,9 @@ import "tailwindcss/tailwind.css";
 import "element-plus/theme-chalk/src/message.scss";
 import "./styles/index.scss";
 
-import * as ElementPlusIconsVue from "@element-plus/icons-vue";
-
 import VueLatex from "vatex";
 
 const app = createApp(App);
 app.use(router).use(ElementPlus).use(VueLatex);
-console.log(app);
-
-for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-  app.component(key, component);
-}
 
 app.mount("#app");
