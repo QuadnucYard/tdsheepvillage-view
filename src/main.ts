bd04943ebb3a677faafa1acc1ec2358b9e31da0d
QuadnucYard <2380433991@qq.com>
2023-11-06 15:16:45 +0800

    Remove ElementPlus global import

--- src/main.ts
+++ src/main.ts
@@ -2,8 +2,6 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
 
-import ElementPlus from "element-plus";
-
 import "tailwindcss/tailwind.css";
 import "element-plus/theme-chalk/src/message.scss";
 import "./styles/index.scss";
@@ -11,6 +9,6 @@ import "./styles/index.scss";
 import VueLatex from "vatex";
 
 const app = createApp(App);
-app.use(router).use(ElementPlus).use(VueLatex);
+app.use(router).use(VueLatex);
 
 app.mount("#app");
