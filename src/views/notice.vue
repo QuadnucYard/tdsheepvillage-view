4e04efb9bb2208ec0b0deacb35f039346b2b636a
QuadnucYard <2380433991@qq.com>
2024-02-08 23:43:44 +0800

    Remove notice

--- src/views/notice.vue
+++ src/views/notice.vue
@@ -1,15 +1,9 @@
 <template>
   <el-tabs>
-    <el-tab-pane label="设计大赛">
-      <el-card>
-        <component :is="DesignContest" />
-      </el-card>
-    </el-tab-pane>
   </el-tabs>
 </template>
 
 <script setup lang="ts">
-import DesignContest from "@/assets/docs/design-contest.md";
 </script>
 
 <style scoped></style>
