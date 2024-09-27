8482774a43b4829c37fe993c62a16d98a4d3d5c9
QuadnucYard <2380433991@qq.com>
2024-09-27 21:00:01 +0800

    fix: wiki color theme, not work

--- src/views/wiki.vue
+++ src/views/wiki.vue
@@ -1,11 +1,15 @@
 <template>
   <el-card>
-    <component :is="Wiki" color-theme="dark" />
+    <component :is="Wiki" :color-theme="isDark ? 'dark' : 'light'" />
   </el-card>
 </template>
 
 <script setup lang="ts">
+import { useDark } from "@vueuse/core";
+
 import Wiki from "@/assets/docs/wiki.md";
+
+const isDark = useDark();
 </script>
 
 <style scoped></style>
